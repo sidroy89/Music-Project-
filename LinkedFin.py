e1663118781637c90d98f16df0d28038f34ced55
sidroy89 <84402810+sidroy89@users.noreply.github.com>
2023-07-12 17:56:24 +0530

    Update LinkedFin.py

--- LinkedFin.py
+++ LinkedFin.py
@@ -21,36 +21,18 @@ import base64
 
 
 
-st.set_page_config(layout="wide")
-
-video_html = """
-		<style>
-
-		#myVideo {
-		  position: fixed;
-		  right: 0;
-		  bottom: 0;
-		  min-width: 100%; 
-		  min-height: 100%;
-		}
-
-		.content {
-		  position: fixed;
-		  bottom: 0;
-		  background: rgba(0, 0, 0, 0.5);
-		  color: #f1f1f1;
-		  width: 100%;
-		  padding: 20px;
-		}
-
-		</style>	
-		<video autoplay muted loop id="myVideo">
-		  <source src="https://static.streamlit.io/examples/star.mp4")>
-		  Your browser does not support HTML5 video.
-		</video>
-        """
-
-st.markdown(video_html, unsafe_allow_html=True)
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://media.tenor.com/jIx6LIssomUAAAAC/vinyl-anime.gif");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 
 
 st.header("Welcome to Sound Sage V-1.0.0")
