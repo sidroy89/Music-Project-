87248aa9370553ee08689db84d4fb7cba270ffc9
sidroy89 <84402810+sidroy89@users.noreply.github.com>
2023-07-12 18:41:01 +0530

    Update LinkedFin.py

--- LinkedFin.py
+++ LinkedFin.py
@@ -40,7 +40,7 @@ st.header("Welcome to Sound Sage V-1.0.0")
 
 
 st.markdown(f"""
-<div>
+<div class="intro">
 <p style= background-color:#7894c2 border-radius:25px border:black> Welcome to SoundSge. Your guide to composing your own music. 
 SoundSage will analyse the songs you give it and find out what makes those songs sound unique and help you compose similar-sounding music. 
 </p>
